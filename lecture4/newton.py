a094678644bfcaba3ee07881c6c0d2df8c7bb13e
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-14 18:50:24 +0700

    Update newton.py

--- lecture4/newton.py
+++ lecture4/newton.py
@@ -4,5 +4,5 @@ from scipy import optimize as o
 def f(x):
 	return x**3 - 4*x**2 - 4*x + 16
 
-root = float(input('Input'))
+root = float(input('Input: '))
 print('root is {}'.format(o.newton(f,root)))
