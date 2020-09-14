5572be16e05bbf638fffb5e8a0424ceedacdf220
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-14 16:03:14 +0700

    task4

---
+++ lecture4/Task4.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 15:22:31 2020
+
+@author: User
+"""
+
+
+import math as m
+
+def f(x):
+    return x**3 - 4*x**2 - 4*x + 16
+
+
+def df(x):
+    return 3*x**2 - 8*x -4
+
+
+def g(x):
+    # f(x) = e^(-x) - x
+    # x = e^(-x)
+    return m.e**(-x)
+
+
+def newton(f,df,x0,p):
+    es = 0.5 *(10**(2-p))
+    x = prev_x = x0
+    i = 0
+    ea = 100
+    while ea > es:
+        x = x - (f(x)/df(x))
+        ea = abs((x-prev_x)/x) * 100
+        prev_x = x
+        i += 1
+        print(x)
+
+
+def fixedpoint(g,df,x0,p):
+    es = 0.5 *(10**(2-p))
+    x = prev_x = x0
+    i = 0
+    ea = 100
+    while ea > es:
+        x = g(x)
+        ea = abs((x-prev_x)/x) * 100
+        prev_x = x
+        i += 1
+        print(x)
+
+
+# newton(f,df,4.2,4)
+# fixedpoint(g,df,2,4)
+
+    
--- lecture4/newton.py
+++ lecture4/newton.py
@@ -2,7 +2,16 @@
 from scipy import optimize as o
 
 def f(x):
+<<<<<<< Updated upstream
 	return x**3 - 4*x**2 - 4*x + 16
 
 root = float(input('Input'))
 print('root is {}'.format(o.newton(f,root)))
+=======
+	return x**3 - 4*x**2 - 4*x + 12
+	
+root = o.newton(f,-1.5)
+
+
+
+>>>>>>> Stashed changes
