02057596382594855d7eb3972e8e641c0928c99b
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-14 18:50:12 +0700

    Update fixedpoint.py

--- lecture4/fixedpoint.py
+++ lecture4/fixedpoint.py
@@ -2,7 +2,7 @@
 from scipy import optimize as o
 import math as m
 def g(x):
-	return m.e**(-x) - 1
+	return m.e**(-x)
 
-root = float(input('Input'))
+root = float(input('Input: '))
 print('root is {}'.format(o.fixed_point(g,root)))
