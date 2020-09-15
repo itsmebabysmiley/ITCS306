b48a9e658128a0f55842c9867d10cb697764e47e
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-15 22:51:17 +0700

    change f(x) doesn't matter

--- lecture4/newton.py
+++ lecture4/newton.py
@@ -1,8 +1,8 @@
 #just python3 newton.py
 from scipy import optimize as o
-
+import numpy 
 def f(x):
-	return x**3 - 4*x**2 - 4*x + 16
+	return numpy.sin(numpy.sqrt(x)) - x
 
 root = float(input('Input: '))
 print('root is {}'.format(o.newton(f,root)))
