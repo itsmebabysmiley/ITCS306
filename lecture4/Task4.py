d959f22502cefe8baca6ce6264f209ce56a4a5f4
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-25 11:59:42 +0700

    Update Task4.py

--- lecture4/Task4.py
+++ lecture4/Task4.py
@@ -9,18 +9,30 @@ Created on Mon Sep 14 15:22:31 2020
 import math as m
 
 def f(x):
-    return x**3 - 4*x**2 - 4*x + 16
-
+    # task1 x**3 - 4*x**2 - 4*x + 16
+    # task2 x**2 - 2*x -3
+    # task2 m.e**(-x) - x
+    # task2 m.sin(2*m.pi*x) + m.e**(1.2*x) + x - 2.5
+    return  m.sin(2*m.pi*x) + m.e**(1.2*x) + x - 2.5
 
 def df(x):
-    return 3*x**2 - 8*x -4
+    # 1.2*m.e**(1.2*x) + 2*m.pi*m.cos(2*m.pi*x)+1
+
+    # task1 3*x**2 - 8*x -4
+    # task2 2*(x-1)
+    # task2 -1 * m.e**(-x) - 1
+    # task2 1.2*m.e**(1.2*x) + 2*m.pi*m.cos(2*m.pi*x)+1
+    return 1.2*m.e**(1.2*x) + 2*m.pi*m.cos(2*m.pi*x)+1
 
 
 def g(x):
     # f(x) = e^(-x) - x
     # x = e^(-x)
-    return m.e**(-x)
-
+    # m.sin(m.sqrt(x))
+    # 2.5 - m.sin(2*m.pi) + m.e**(1.2*x)
+    # m.sqrt(2*x+3)
+    # m.e**(-x)
+    return 2.5 - m.sin(2*m.pi) - m.e**(1.2*x)
 
 def newton(f,df,x0,p):
     es = 0.5 *(10**(2-p))
